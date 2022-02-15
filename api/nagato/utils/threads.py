a46496ac1ce56542f590f9875ca45cf239ed6aea
Nathan PERIER <periernathan2000@gmail.com>
2022-02-15 19:28:34 +0100

    Added a way to cancel a download and to get a download's state

--- api/nagato/utils/threads.py
+++ api/nagato/utils/threads.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 from nagato.utils.compression import Archiver
 
 import time
@@ -10,7 +11,7 @@ import traceback
 
 _active_downloads = {}
 
-_downloads_status = {}
+_completed_downloads = {}
 
 _executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix='nagato_dl')
 
@@ -22,14 +23,14 @@ class DownloadState(Enum) :
 	SAVING     = 3
 	COMPLETE   = 4
 	FAILED     = -1
+	CANCELLED  = -2
 
 
-def _generateId(filename) :
+def _generateId(t, filename) :
 	for _ in range(10) :
-		t = time.time()
 		digest = hashlib.md5(f"{t}-{filename}".encode()).digest()
 		h = b64encode(digest, b'-_')[:-2].decode('utf-8')
-		if h not in _downloads_status :
+		if h not in _active_downloads and h not in _completed_downloads :
 			return h
 	raise RuntimeError("Could not attribute and ID to a download thread")
 
@@ -40,28 +41,66 @@ class ChapterDownload :
 		self._downloader = downloader
 		self._chapter = chapter_id
 		self._archiver : Archiver = downloader.getArchiver(chapter_id)
-		self._id = _generateId(self._archiver.getFilename())
+		self._creation = time.time()
+		self._id = _generateId(self._creation, self._archiver.getFilename())
 		self._status = DownloadState.CREATED
 		self._future = None
+		self._begin = None
+		self._end = None
+		_active_downloads[self._id] = self
 	
 	def submit(self) :
+		self._creation = time.time() # Update the creation date to be the time of submission (just in case)
 		self._status = DownloadState.QUEUED
 		self._future = _executor.submit(self.perform)
+		self._future.add_done_callback(self.after)
+		logger.info('Download {} submitted', self._id)
 		return self._id
 
 	def perform(self) :
 		try :
+			self._begin = time.time()
 			self._status = DownloadState.PROCESSING
 			with self.getArchiver() as archiver :
 				self._downloader.downloadChapter(self._chapter, archiver)
 				self._status = DownloadState.SAVING
 			self._status = DownloadState.COMPLETE
 		except Exception :
-			print(traceback.format_exc())
+			logger.error('Error in download {}:\n{}', self._id, traceback.format_exc())
 			self._status = DownloadState.FAILED
 	
+	def after(self, _=None) :
+		logger.info('Download {} exited with status {}', self._id, self._status)
+		self._end = time.time()
+		del _active_downloads[self._id]
+		_completed_downloads[self._id] = self.getState()
+	
+	def getState(self) -> dict :
+		res = {
+			'file': self._archiver.getFilename(),
+			'status': str(self._status),
+			'completion': self._archiver.getProgress(),
+			'created': self._creation
+		}
+		if self._begin is not None :
+			res['begin'] = self._begin
+		if self._end is not None :
+			res['end'] = self._end
+		return res
+	
+	def cancel(self) :
+		res = self._future.cancel()
+		if res :
+			self._status = DownloadState.CANCELLED
+			self.after()
+		return res
+
 	def getArchiver(self) -> Archiver :
 		return self._archiver
 	
 	def getFuture(self) -> Future :
 		return self._future
+
+
+def clearHistory() :
+	_completed_downloads.clear()
