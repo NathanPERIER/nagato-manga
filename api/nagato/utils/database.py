7eb5c1db23d5bdc9abe7989158f685db40d29b9b
Nathan PERIER <periernathan2000@gmail.com>
2022-02-27 12:07:09 +0100

    Added classes to manipulate database entries

--- api/nagato/utils/database.py
+++ api/nagato/utils/database.py
@@ -1,4 +1,5 @@
 
+from enum import Enum
 from nagato.utils import config
 
 import os
@@ -29,6 +30,66 @@ class SqlConnection :
 def tableExists(cur: sqlite3.Cursor, table_name: str) -> bool :
 	l = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [table_name]).fetchall()
 	return len(l) > 0
+
+
+class SqlMangaEntry :
+
+	def __init__(self, site: str, manga_id: str) :
+		self._site = site
+		self._id = manga_id
+	
+	def isStarred(self, cur: sqlite3.Cursor) -> bool :
+		c = cur.execute("SELECT * FROM mangas WHERE site=? and id=?", [self._site, self._id])
+		return len(c.fetchall()) > 0
+	
+	def star(self, cur: sqlite3.Cursor) -> bool :
+		if self.isStarred(cur) :
+			return False
+		cur.execute("INSERT INTO mangas VALUES (?, ?)", [self._site, self._id])
+		return True
+	
+	def unstar(self, cur: sqlite3.Cursor) -> bool :
+		if not self.isStarred(cur) :
+			return False
+		cur.execute("DELETE FROM mangas WHERE site=? and id=?", [self._site, self._id])
+		return True
+	
+	def getChaptersWithTags(self, cur: sqlite3.Cursor) -> "dict[str,str]" :
+		l = cur.execute("SELECT id, mark FROM chapters WHERE site=? and manga=?", [self._site, self._id]).fetchall()
+		return {e[0]: e[1] for e in l}
+
+
+class ChapterMark(Enum) :
+	DOWNLOADED = 'D'
+	IGNORED    = 'I'
+
+class SqlChapterEntry :
+	
+	def __init__(self, site: str, chapter_id: str, manga_id: str) :
+		self._site = site
+		self._id = chapter_id
+		self._manga = manga_id
+
+	def exists(self, cur: sqlite3.Cursor) -> bool :
+		return self.getMark(cur) is not None
+	
+	def getMark(self, cur: sqlite3.Cursor) :
+		l = cur.execute("SELECT mark FROM chapters WHERE site=? and id=? and manga=?", [self._site, self._id, self._manga]).fetchall()
+		return ChapterMark(l[0]) if len(l) > 0 else None
+
+	def setMark(self, cur: sqlite3.Cursor, mark: ChapterMark) -> bool :
+		chapter_exists = self.exists(cur)
+		if mark is not None and chapter_exists :
+			cur.execute("UPDATE chapters SET mark=? WHERE site=? and id=? and manga=?", [mark.value, self._site, self._id, self._manga])
+			return True
+		if mark is not None and not chapter_exists :
+			cur.execute("INSERT INTO chapters VALUES (?, ?, ?, ?)", [self._site, self._id, self._manga, mark.value])
+			return True
+		if mark is None and chapter_exists :
+			cur.execute("DELETE FROM chapters WHERE site=? and id=? and manga=?", [self._site, self._id, self._manga])
+			return True
+		return False
+
 
 with SqlConnection(__database_path) as con :
 
@@ -61,4 +122,3 @@ with SqlConnection(__database_path) as con :
 		)
 
 	con.commit()
-
