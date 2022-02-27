25561ee86b0983b4fb9bf41df0817068e0af699f
Nathan PERIER <periernathan2000@gmail.com>
2022-02-27 13:11:47 +0100

    The manga identifier is only fetched when necessary

--- api/nagato/utils/database.py
+++ api/nagato/utils/database.py
@@ -27,6 +27,9 @@ class SqlConnection :
 		if exc_type is not None :
 			return False
 
+def getConnection() :
+	return SqlConnection(__database_path)
+
 def tableExists(cur: sqlite3.Cursor, table_name: str) -> bool :
 	l = cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [table_name]).fetchall()
 	return len(l) > 0
@@ -75,33 +78,34 @@ class ChapterMark(Enum) :
 
 class SqlChapterEntry :
 	
-	def __init__(self, site: str, chapter_id: str, manga_id: str) :
+	def __init__(self, site: str, chapter_id: str, downloader) :
 		self._site = site
 		self._id = chapter_id
-		self._manga = manga_id
+		self._dl = downloader
 
 	def exists(self, cur: sqlite3.Cursor) -> bool :
 		return self.getMark(cur) is not None
 	
 	def getMark(self, cur: sqlite3.Cursor) :
-		l = cur.execute("SELECT mark FROM chapters WHERE site=? and id=? and manga=?", [self._site, self._id, self._manga]).fetchall()
+		l = cur.execute("SELECT mark FROM chapters WHERE site=? and id=?", [self._site, self._id]).fetchall()
 		return ChapterMark(l[0]) if len(l) > 0 else None
 
 	def setMark(self, cur: sqlite3.Cursor, mark: ChapterMark) -> bool :
 		chapter_exists = self.exists(cur)
 		if mark is not None and chapter_exists :
-			cur.execute("UPDATE chapters SET mark=? WHERE site=? and id=? and manga=?", [mark.value, self._site, self._id, self._manga])
+			cur.execute("UPDATE chapters SET mark=? WHERE site=? and id=?", [mark.value, self._site, self._id])
 			return True
 		if mark is not None and not chapter_exists :
-			cur.execute("INSERT INTO chapters VALUES (?, ?, ?, ?)", [self._site, self._id, self._manga, mark.value])
+			manga = self._dl.getMangaForChapter(self._id)
+			cur.execute("INSERT INTO chapters VALUES (?, ?, ?, ?)", [self._site, self._id, manga, mark.value])
 			return True
 		if mark is None and chapter_exists :
-			cur.execute("DELETE FROM chapters WHERE site=? and id=? and manga=?", [self._site, self._id, self._manga])
+			cur.execute("DELETE FROM chapters WHERE site=? and id=?", [self._site, self._id])
 			return True
 		return False
 
 
-with SqlConnection(__database_path) as con :
+with getConnection() as con :
 
 	cur = con.cursor()
 
