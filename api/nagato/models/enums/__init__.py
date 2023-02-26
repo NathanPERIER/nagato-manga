34b12433ca04d9b50b989dc404f725d8ca3823c2
Nathan PERIER <periernathan2000@gmail.com>
2023-02-26 14:35:48 +0100

    Added an enum for demographics

--- api/nagato/models/enums/__init__.py
+++ api/nagato/models/enums/__init__.py
@@ -1,3 +1,4 @@
 from .status       import Status
 from .rating       import Rating
 from .chapter_mark import ChapterMark
+from .demographic  import Demographic
---
+++ api/nagato/models/enums/demographic.py
@@ -0,0 +1,9 @@
+
+from .dataenum import DataEnum
+
+class Demographic(DataEnum) :
+    UNKNOWN = 0
+    SHOUNEN = 1
+    SHOUJO  = 2
+    SEINEN  = 3
+    JOSEI   = 4
