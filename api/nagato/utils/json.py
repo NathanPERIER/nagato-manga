cf453528249f6f87b3aa340309baef3bf4d96d7d
Nathan PERIER <periernathan2000@gmail.com>
2023-03-18 12:49:51 +0100

    Added JSON serialisation compatible with DataObjects

--- api/nagato/utils/json.py
+++ api/nagato/utils/json.py
@@ -3,7 +3,7 @@ from nagato.models.dataobj import DataObject, ExtJson, RedExtJson
 
 import json
 
-from typing import Union, Sequence, Mapping
+from typing import Union, Sequence, Mapping, Any
 # TODO TypeAlias available in Python 3.10
 
 # Deserialised JSON
@@ -17,6 +17,17 @@ def loadJson(path: str) -> Json :
 	with open(path, 'r') as f :
 		return json.load(f)
 
-# TODO with a custom serialiser for DataObjects
-# def dumpJson(data: ExtJson) -> str :
-# def saveJson(data: ExtJson, path: str) :
+
+class DataObjectEncoder(json.JSONEncoder) :
+
+	def default(self, o: Any) -> Any :
+		if issubclass(type(o), DataObject) :
+			return o.toJson()
+		return super().default(o)
+
+def dumpJson(data: ExtJson) -> str :
+	json.dumps(data, cls=DataObjectEncoder)
+
+def saveJson(data: ExtJson, path: str) :
+	with open(path, 'w') as f :
+		json.dump(data, f, cls=DataObjectEncoder)
